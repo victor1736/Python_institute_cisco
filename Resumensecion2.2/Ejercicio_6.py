0c408ffabef1281e9dc0b505acb5ae2b0eb413d0
victor1736 <victoralfonsofernandezhoyos@gmail.com>
2022-07-07 13:00:59 -0500

    Se agrega el proyecto

---
+++ Resumensecion2.2/Ejercicio_6.py
@@ -0,0 +1,13 @@
+'''
+¿Cuál es la salida del siguiente código?
+
+R//
+0123
+4
+'''
+n = range(4)
+
+for num in n:
+    print(num - 1)
+else:
+    print(num)
---
+++ Resumensecion2.2/Ejercicio_7.py
@@ -0,0 +1,9 @@
+'''
+¿Cuál es la salida del siguiente código?
+
+R//
+0
+3
+'''
+for i in range(0, 6, 3):
+    print(i)

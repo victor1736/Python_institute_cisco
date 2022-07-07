2b494389acd4c85ff99b5ee30d6c0257f09ff3c5
victor1736 <82006043+victor1736@users.noreply.github.com>
2022-07-07 08:59:16 -0500

    Se agrega el proyecto

---
+++ Resumensecion2.2/Ejercicio_1.py
@@ -0,0 +1,7 @@
+'''
+Crea un bucle for que cuente de 0 a 10, e imprima números impares en la pantalla. Usa el esqueleto de abajo:
+
+'''
+for i in range(0,11):
+    if (i % 2 != 0):
+        print(i)

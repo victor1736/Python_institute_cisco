8247c6ba3252eeec178d5b7d2b5d2c7789fab245
victor1736 <victoralfonsofernandezhoyos@gmail.com>
2022-07-22 15:09:27 -0500

    ejercicio de modulo 2

---
+++ python_2/Modulo_2/Tu_Propio_slit.py
@@ -0,0 +1,22 @@
+'''
+Ya sabes como funiona el método split(). Ahora queremos que lo pruebes.
+
+Tu tarea es escribir tu propia función, que se comporte casi como el método original split(), por ejemplo:
+
+Debe aceptar únicamente un argumento: una cadena.
+Debe devolver una lista de palabras creadas a partir de la cadena, dividida en los lugares donde la cadena contiene espacios en blanco.
+Si la cadena está vacía, la función debería devolver una lista vacía.
+Su nombre debe ser mysplit().
+Utiliza la plantilla en el editor. Prueba tu código con cuidado.
+'''
+def mysplit(strng):
+    strng = input(str('Ingrese la cadena de su preferencia: '))
+    if 
+                  
+
+
+print(mysplit("Ser o no ser, esa es la pregunta"))
+print(mysplit("Ser o no ser,esa es la pregunta"))
+print(mysplit("   "))
+print(mysplit(" abc "))
+print(mysplit(""))

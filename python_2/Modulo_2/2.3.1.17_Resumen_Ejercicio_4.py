c153b3fb180a717cd8d01d0f794d3ccbef70cb77
victor1736 <victoralfonsofernandezhoyos@gmail.com>
2022-07-24 13:40:17 -0500

    "modulo dos Resumen uno termino"

---
+++ python_2/Modulo_2/2.3.1.17_Resumen_Ejercicio_4.py
@@ -0,0 +1,9 @@
+'''
+¿Cuál es el resultado esperado del siguiente código?
+
+R//Imprime Es difícil o posible
+
+'''
+s = 'Es fácil o imposible'
+s = s.replace('fácil', 'difícil').replace('im', '')
+print(s)
